d25e378235404696219b03381065bf5b7e9fd958
sanjay123-py <sanjaysanjay1270@gmail.com>
2023-04-20 10:02:54 +0530

    addup

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask,render_template,redirect,flash,request
 from flask_cors import CORS, cross_origin
 import pickle
 import numpy as np
-app=Flask(__name__)
+import pandas as pd
+app = Flask(__name__, template_folder='./Templates')
 
 CORS(app)
 @app.route("/")
@@ -10,27 +11,30 @@ def home():
     return render_template('index.html')
 @app.route("/predict",methods=['POST','GET'])
 def predict():
-    try:
-        if request.method=='POST':
-            age=request.form.get('age')
-            gender=request.form.get("gender")
-            family_history=request.form.get('family_history')
-            benifits=request.form.get('benefits')
-            care_options=request.form.get('care_options')
-            anonymity=request.form.get('anonymity')
-            leave=request.form.get('leave')
-            work_interfere=request.form.get('work_interfere')
-            feature_list=np.expand_dims(np.array([age,gender,family_history,benifits,care_options,anonymity,leave,work_interfere]),axis=0)
-            feature_list=feature_list.astype(int)
-            with open('boostmodel.pkl','rb') as f:
-                model=pickle.load(f)
-            prediction=model.predict(feature_list)
-            print(prediction)
-            return "{}".format(prediction)
-        else:
-            return render_template('index.html')
-    except Exception as e:
-        print(e)
+    age = request.form.get('age')
+    gender = request.form.get('gender')
+    family_history = request.form.get('family_history')
+    benefits = request.form.get('benefits')
+    care_options = request.form.get('care_options')
+    anonymity = request.form.get('anonymity')
+    leave = request.form.get('leave')
+    work_interfere = request.form.get('work_interfere')
+
+    # Reshape input into a 2D array
+    input_data = np.array(
+        [age, gender, family_history, benefits, care_options, anonymity, leave, work_interfere]).reshape(1, -1)
+
+    # Load the model
+    with open('boostmodel.pkl', 'rb') as f:
+        model = pickle.load(f)
+
+    # Make prediction and return result
+    prediction = model.predict(input_data)
+    if str(prediction)=="1":
+        VALUE="Person does have illness"
+    else:
+        VALUE="Person doesnot have illness"
+    return render_template("index.html",VALUE=VALUE)
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=1234, debug=True)
+    app.run(host='0.0.0.0', port=12234, debug=True)
